6da3a05198b96b2a571c20a4ae75f14a3e073fd4
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-08-17 00:02:33 -0300

    Update [id].tsx

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -17,7 +17,7 @@ export default function MovieDetails({ movie }: MovieDetailsProps) {
       <h1>{movie.title}</h1>
       <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
       <p>{movie.overview}</p>
-      <p>Data de Lançamento: {movie.release_date}</p>
+      <p>Data De Lançamento: {movie.release_date}</p>
     </div>
   );
 }

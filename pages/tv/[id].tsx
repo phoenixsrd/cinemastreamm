5a17521218b4379f605a22216fa2acc56aef2ecd
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-08-17 00:08:52 -0300

    Create [id].tsx

---
+++ pages/tv/[id].tsx
@@ -0,0 +1,35 @@
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
+
+type TVShowDetailsProps = {
+  tvShow: {
+    id: number;
+    name: string;
+    poster_path: string;
+    overview: string;
+    first_air_date: string;
+  };
+};
+
+export default function TVShowDetails({ tvShow }: TVShowDetailsProps) {
+  return (
+    <div>
+      <h1>{tvShow.name}</h1>
+      <img src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`} alt={tvShow.name} />
+      <p>{tvShow.overview}</p>
+      <p>Data de Estreia: {tvShow.first_air_date}</p>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const API_KEY = 'cc2577ef867decbe177dea0f28d5f028';
+  const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}`);
+
+  return {
+    props: {
+      tvShow: response.data,
+    },
+  };
+};
